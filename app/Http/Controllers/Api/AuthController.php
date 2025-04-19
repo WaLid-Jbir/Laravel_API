c44b364c3b9aff2a18196b9b39727abc03fe4f8b
WaLid-Jbir <anawalid737@gmail.com>
2025-04-19 16:45:57 +0100

    Setting up api file with Authentication controller fo Sanctum

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    /**
+     * Register a new user.
+     */
+    public function regiter(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
