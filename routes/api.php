6c42aa93658c63f721b11492950f30543308a192
WaLid-Jbir <anawalid737@gmail.com>
2025-04-24 01:16:07 +0100

    Get User Profile information function endpoint

--- routes/api.php
+++ routes/api.php
@@ -11,4 +11,5 @@ Route::get('/user', function (Request $request) {
 Route::controller(AuthController::class)->group(function(){
     Route::post('/register', 'register');
     Route::post('/login', 'login');
+    Route::get('/user', 'userProfile')->middleware('auth:sanctum');
 });
